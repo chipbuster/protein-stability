5ae7e1857ac416b02848893223073145ef733a48
Kevin Song <chipbuster@gmail.com>
2019-01-22 13:47:49 -0600

    Copy fep-gen from earlier commit 2da1c3f

---
+++ entropy-measure/proteins/fep-gen/mutate_protein.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Need following arguments: PDB ID, Mutation ID String, target residue number,
+#                           target residue mutation
+
+
+# Change this to change parameter location
+PARAMDIR="/work/02711/chipbus/stampede2/data/namd-auxfiles/c36_params"
+
+if [ -z  "${4-}" ]; then
+    echo "Usage: $0 <pdbid> <mutid string> <residue number> <mutation 3let code>"
+    exit 1
+fi
+
+PDBID="$1"
+MUTID="$2"
+RESNM="$3"
+MUTCD="$4"
+
+# Set up mutations
+vmd -dispdev text -e ./automutate.tcl -args "$PDBID" "$MUTID" "$RESNM" "$MUTCD"
+
+# Patch Dimensions
+python2 ./patchdim.py
+
+# Patch others
+for file in "*.namd"; do
+    sed -i "s#MUTNAME#$PDBID-$MUTID#" $file
+    sed -i "s#PARAMETER_DIR#$PARAMDIR#" $file
+done
+
+mv *.namd prepared
+cp namd-templates/*.slurm prepared
---
+++ entropy-measure/proteins/fep-gen/patchdim.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# Patch dimensions in a NAMD runfile
+
+import re
+import sys,os
+from subprocess import call
+
+infile = open('dimensions.out','r')
+
+try:
+    (a,b) = infile.readlines()
+except e:
+    print("dimensions.out seems to have been edited. It should only have two lines")
+    print(e)
+    sys.exit(1)
+
+# Delete curlybraces
+a = a.replace("{","")
+a = a.replace("}","")
+b = b.replace("{","")
+b = b.replace("}","")
+
+a = a.split()
+b = b.split()
+
+# Get numbers
+xmin = float(a[2])
+ymin = float(a[3])
+zmin = float(a[4])
+xmax = float(a[5])
+ymax = float(a[6])
+zmax = float(a[7])
+
+xcent = float(b[2])
+ycent = float(b[3])
+zcent = float(b[4])
+
+xsz = xmax - xmin
+ysz = ymax - ymin
+zsz = zmax - zmin
+
+cb1string = "cellBasisVector1     %f 0. 0."%xsz
+cb2string = "cellBasisVector2     0. %f 0."%ysz
+cb3string = "cellBasisVector3     0. 0. %f"%zsz
+ccstring  = "cellOrigin           %f %f %f"%(xcent,ycent,zcent)
+
+#print cb1string
+#print cb2string
+#print cb3string
+#print ccstring
+
+for fprefix in ['equilibrate','forward','backward']:
+    fname = os.path.join('namd-templates', fprefix + ".namd.template")
+    outfname = fprefix + ".namd"
+    f = open(outfname,'w')
+    call(['sed', "s/REPLACE_CELL_BASIS_1/%s/g"%cb1string, fname], stdout=f)
+    call(['sed', "-i", "s/REPLACE_CELL_BASIS_2/%s/g"%cb2string,outfname])
+    call(['sed', "-i", "s/REPLACE_CELL_BASIS_3/%s/g"%cb3string,outfname])
+    call(['sed', "-i", "s/REPLACE_CELL_ORIGIN/%s/g"%ccstring,outfname])
