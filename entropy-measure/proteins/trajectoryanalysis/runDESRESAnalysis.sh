cac526860398fe14183306821cae18922c442bb7
Kevin Song <chipbuster@gmail.com>
2019-02-20 19:21:20 -0600

    First draft of running analysis

---
+++ entropy-measure/proteins/trajectoryanalysis/runDESRESAnalysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DATADIR=/home/chipbuster/Spinny/PDBStorage/piana2012
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT="$SCRIPTDIR/replicatearxiv.py"
+
+find $DATADIR -mindepth 1 -maxdepth 1 -type d -print0 | while IFS= read -r -d $'\0' PROTNAME_RAW; do
+    PROTNAME=$(basename $PROTNAME_RAW)
+    STAGE1="DESRES-Trajectory_$PROTNAME-protein"
+    STAGE2="$PROTNAME-protein"
+    FULLPATH="$DATADIR/$PROTNAME/$STAGE1/$STAGE2"
+    cd "$FULLPATH"
+
+    $SCRIPT $PROTNAME-protein.pdb $PROTNAME-*.dcd
+
+    mv "$SCRIPTDIR/arxivoutput.txt" $SCRIPTDIR/$PROTNAME.txt
+done
