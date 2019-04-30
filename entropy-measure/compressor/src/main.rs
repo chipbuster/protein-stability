60790c30cc758c67dfc2bea8651921199cd9bc28
Kevin Song <chipbuster@gmail.com>
2019-04-29 19:36:50 -0500

    Add Rust Huffman Compressor

---
+++ entropy-measure/compressor/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs;
+use std::process;
+
+use compressor;
+use bitvec::BitVec;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <infilename>", &args[0]);
+        process::exit(1);
+    }
+
+    let infilename = &args[1];
+    let contents = fs::read_to_string(infilename).expect("Something went wrong reading the file").into_bytes();
+
+    let coder = compressor::HuffEncoder::new(&contents);
+    let encoded = coder.encode(&contents);
+
+    println!("Ratio is {}", (encoded.len() as f64 / 8.0) / contents.len() as f64 );
+}
