64a8cf5d3b325470a5483820063dc4dcaba96568
Kevin Song <chipbuster@gmail.com>
2019-12-10 16:21:51 -0600

    Ability to create datasets

--- protein-tools/datarep/core.jl
+++ protein-tools/datarep/core.jl
@@ -24,7 +24,7 @@ end
 
 ParameterizedData(fp::String, dp::String) = begin
     file = h5open(fp, "cw")
-    dat  = d_open[dp * "/parameterized"]
+    dat  = file[dp * "/parameterized"]
     ParameterizedData(dat, fp, dp, file)
 end
 
@@ -38,7 +38,44 @@ end
 
 BinnedData(fp::String, dp::String) = begin
     file = h5open(fp, "cw")
-    dat  = d_open(dp * "/binned")
+    dat  = file[dp * "/binned"]
     ParameterizedData(dat, fp, dp, file)
 end
 
+function close(data::T) where T <: AbstractSimData
+    close(data.h5File)
+end
+
+"""Create a dataset with the specified data, backed by an HDF5 file."""
+function create_inputdata(fp::String, dp::String, data, attr)
+    file = h5open(fp, "cw")
+    file[dp * "/inputdata"] = data
+    fdata = file[dp * "/inputdata"]
+    print(typeof(fdata))
+    for (k, v) in attr
+        attrs(fdata)[k] = v
+    end
+    InputData(fdata, fp, dp, file)
+end
+
+function create_parameterizeddata(fp::String, dp::String, data, attr)
+    file = h5open(fp, "cw")
+    file[dp * "/parameterized"] = data
+    fdata = file[dp * "/parameterized"]
+    print(typeof(fdata))
+    for (k, v) in attr
+        attrs(fdata)[k] = v
+    end
+    ParameterizedData(fdata, fp, dp, file)
+end
+
+function create_binneddata(fp::String, dp::String, data, attr)
+    file = h5open(fp, "cw")
+    file[dp * "/binned"] = data
+    fdata = file[dp * "/binned"]
+    print(typeof(fdata))
+    for (k, v) in attr
+        attrs(fdata)[k] = v
+    end
+    BinnedData(fdata, fp, dp, file)
+end
