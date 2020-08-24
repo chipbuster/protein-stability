5dcee8906d40827bd36c01c074c1dc798899b8ca
Kevin Song <chips@ksong.dev>
2020-08-24 13:35:31 -0500

    Add eta computation functions

--- protein-tools/core/compressor.jl
+++ protein-tools/core/compressor.jl
@@ -1,4 +1,5 @@
 using Libdl;
+using LinearAlgebra;
 
 function get_compressor_function()
     libpath = joinpath(@__DIR__, "simplelzma.so")
@@ -14,9 +15,28 @@ function get_compressor_function()
     dlsym(lib, :get_compressed_size)
 end
 
-function compress_data(data::Vector{UInt8})
+function compressed_size(data::Vector{UInt8})
     len = length(data)
     func = get_compressor_function()
     csize = ccall(func, Csize_t, (Ptr{UInt8}, Csize_t), data, len)
     convert(Int, csize)
+end
+
+function compression_eta(data::Vector{UInt8}, random::Union{Vector{UInt8},Nothing}=nothing)
+    """Computes the compression η of the given data.
+    
+    If random is `nothing` (the default), then the "default" random distribution
+    of uniform on UInt8 is used. Otherwise, specified distribution is used."""
+    numel = length(data)
+    if random === nothing
+        random = rand(UInt8, numel)
+    else
+        if (length(random) != numel)
+            printstyled("[WARN]: Random/input data are not the same length!\n", 
+                         bold=true, color=:yellow)
+        end
+    end
+    z = zeros(UInt8, numel)
+    C_0 = compressed_size(z)
+    C_d = compressed_size(data)
 end
