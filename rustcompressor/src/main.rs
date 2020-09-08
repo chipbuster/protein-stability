84dc40e777c89003c2d1ff0ae3c1f04ba90dfe53
Kevin Song <chips@ksong.dev>
2020-09-08 15:18:21 -0500

    Run cargo fmt with new rustfmt

--- rustcompressor/src/main.rs
+++ rustcompressor/src/main.rs
@@ -5,23 +5,23 @@ use std::process;
 use compressor::huff_tree::HuffEncoder;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+  let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        println!("Usage: {} <infilename>", &args[0]);
-        process::exit(1);
-    }
+  if args.len() != 2 {
+    println!("Usage: {} <infilename>", &args[0]);
+    process::exit(1);
+  }
 
-    let infilename = &args[1];
-    let contents = fs::read_to_string(infilename)
-        .expect("Something went wrong reading the file")
-        .into_bytes();
+  let infilename = &args[1];
+  let contents = fs::read_to_string(infilename)
+    .expect("Something went wrong reading the file")
+    .into_bytes();
 
-    let coder = HuffEncoder::new(&contents);
-    let encoded = coder.encode(&contents);
+  let coder = HuffEncoder::new(&contents);
+  let encoded = coder.encode(&contents);
 
-    println!(
-        "Ratio is {}",
-        (encoded.len() as f64 / 8.0) / contents.len() as f64
-    );
+  println!(
+    "Ratio is {}",
+    (encoded.len() as f64 / 8.0) / contents.len() as f64
+  );
 }
--- rustcompressor/src/sliding_window/mod.rs
+++ rustcompressor/src/sliding_window/mod.rs
@@ -5,36 +5,36 @@ use std::iter::Iterator;
 integer types in later experiments.
 */
 struct SlidingWindow<'a, S> {
-    buf: &'a mut dyn Iterator<Item = S>,
-    hashes: VecDeque<usize>,
-    winsz: usize,
+  buf: &'a mut dyn Iterator<Item = S>,
+  hashes: VecDeque<usize>,
+  winsz: usize,
 }
 
 impl<'a, S> SlidingWindow<'a, S>
 where
-    S: Copy,
+  S: Copy,
 {
-    const WIN_SIZE: usize = 1024;
-    const NUM_HASHES: usize = 50;
-    fn new(iter: &'a mut dyn Iterator<Item = S>) -> Self {
-        Self::new_with_params(iter, Self::WIN_SIZE, Self::NUM_HASHES)
-    }
+  const WIN_SIZE: usize = 1024;
+  const NUM_HASHES: usize = 50;
+  fn new(iter: &'a mut dyn Iterator<Item = S>) -> Self {
+    Self::new_with_params(iter, Self::WIN_SIZE, Self::NUM_HASHES)
+  }
 
-    fn new_with_params(iter: &'a mut dyn Iterator<Item = S>, sz: usize, nh: usize) -> Self {
-        Self {
-            buf: iter,
-            hashes: VecDeque::with_capacity(nh),
-            winsz: sz,
-        }
+  fn new_with_params(iter: &'a mut dyn Iterator<Item = S>, sz: usize, nh: usize) -> Self {
+    Self {
+      buf: iter,
+      hashes: VecDeque::with_capacity(nh),
+      winsz: sz,
     }
+  }
 
-    /* Hash function:  hash(i) [(i × 256 ) % 101], taken from wiki */
-    fn next_sym(&mut self) {
-        if let Some(x) = self.buf.next() {
-            if self.hashes.len() == self.winsz {
-                for x in self.hashes.iter_mut() {}
-            }
-        }
-        // If the stream is empty, do nothing.
+  /* Hash function:  hash(i) [(i × 256 ) % 101], taken from wiki */
+  fn next_sym(&mut self) {
+    if let Some(x) = self.buf.next() {
+      if self.hashes.len() == self.winsz {
+        for x in self.hashes.iter_mut() {}
+      }
     }
+    // If the stream is empty, do nothing.
+  }
 }
