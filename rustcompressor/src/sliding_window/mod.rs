98a06466cee06bd7eff1e013bff44db37d03c144
Kevin Song <chips@ksong.dev>
2020-09-08 17:07:00 -0500

    Rough Sliding Window implementation

--- rustcompressor/src/sliding_window/mod.rs
+++ rustcompressor/src/sliding_window/mod.rs
@@ -1,40 +1,69 @@
+use core::cmp::{Eq, PartialEq};
 use std::collections::VecDeque;
 use std::iter::Iterator;
 
 /** Sliding windows always occur on raw inputs, but it might be over larger
 integer types in later experiments.
 */
-struct SlidingWindow<'a, S> {
-  buf: &'a mut dyn Iterator<Item = S>,
-  hashes: VecDeque<usize>,
+pub struct SlidingWindow<'a, S> {
+  source: &'a [S],
+  // hashes: VecDeque<usize>,     // Do not implement until we can prove it's needed
+  window: (usize, usize), // A right-open, i.e. [a, b), pair of indices
   winsz: usize,
 }
 
 impl<'a, S> SlidingWindow<'a, S>
 where
-  S: Copy,
+  S: Copy + PartialEq + Eq,
 {
-  const WIN_SIZE: usize = 1024;
-  const NUM_HASHES: usize = 50;
-  fn new(iter: &'a mut dyn Iterator<Item = S>) -> Self {
-    Self::new_with_params(iter, Self::WIN_SIZE, Self::NUM_HASHES)
+  const DEFAULT_WIN_SIZE: usize = 1024;
+  pub fn new(source: &'a [S]) -> Self {
+    Self::new_with_params(source, Self::DEFAULT_WIN_SIZE)
   }
 
-  fn new_with_params(iter: &'a mut dyn Iterator<Item = S>, sz: usize, nh: usize) -> Self {
+  pub fn new_with_params(source: &'a [S], winsz: usize) -> Self {
+    let win_end = std::cmp::max(winsz, source.len());
     Self {
-      buf: iter,
-      hashes: VecDeque::with_capacity(nh),
-      winsz: sz,
+      source,
+      window: (0, win_end),
+      winsz,
     }
   }
 
-  /* Hash function:  hash(i) [(i × 256 ) % 101], taken from wiki */
-  fn next_sym(&mut self) {
-    if let Some(x) = self.buf.next() {
-      if self.hashes.len() == self.winsz {
-        for x in self.hashes.iter_mut() {}
+  /// Add a new symbol into the sliding window, forcing the old one out if
+  /// necessary.
+  pub fn advance(&mut self) {
+    if self.window.1 < self.source.len() {
+      self.window = (self.window.0 + 1, self.window.1 + 1);
+    }
+  }
+
+  /// Find the longest match in the current sliding window using a brute-force
+  /// search algorithm.
+  pub fn find_match(&self, to_match: &[S]) -> (usize, usize) {
+    let mut backref_dist = 0;
+    let mut backref_size = 0;
+
+    // Start searching at beginning of sliding window.
+    for i in 0..=self.winsz {
+      let dist = self.winsz - i;
+      let match_size = self.longest_match(i, to_match);
+      if match_size > backref_size {
+        backref_dist = i;
+        backref_size = match_size;
       }
     }
-    // If the stream is empty, do nothing.
+
+    (backref_dist, backref_size)
+  }
+
+  /* Dirty index-based searches because Rust iterators are still too hard for
+  my little peanut brain to understand. */
+  fn longest_match(&self, win_index_start: usize, to_match: &[S]) -> usize {
+    let mut offset = 0usize;
+    while self.window[win_index_start + offset] == to_match[offset] {
+      offset += 1;
+    }
+    offset
   }
 }
