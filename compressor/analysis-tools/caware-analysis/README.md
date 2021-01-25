d5536cd4f5c648d8bd4b43eaf365d6b570ce64f5
Kevin Song <chips@ksong.dev>
2021-01-25 12:18:48 -0600

    Split into read/write test binaries

--- compressor/analysis-tools/caware-analysis/README.md
+++
@@ -1,5 +0,0 @@
-# Analysis Software for Content-Aware Compression
-
-Test content aware analysis for compression.
-
-Ideas: sort abs angles by their offset.
---
+++ compressor/rustcompressor/src/bin/test_proto_read.rs
@@ -0,0 +1,29 @@
+use std::{
+  env,
+  io::Read,
+  io::Seek,
+  io::{SeekFrom, Write},
+  process,
+};
+
+use compressor::deflate::proto::dproto::DEFLATEStream;
+use protobuf::Message;
+
+fn main() -> Result<(), std::io::Error> {
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() != 2 {
+    println!("Usage: {} <proto>", &args[0]);
+    process::exit(1);
+  }
+
+  let mut infile = std::fs::File::open(&args[1])
+    .unwrap_or_else(|_| panic!("Could not open input file {}", args[1]));
+
+  let mut cis = protobuf::CodedInputStream::new(&mut infile);
+  let d = DEFLATEStream::parse_from(&mut cis).unwrap();
+
+  println!("{:?}", d);
+
+  Ok(())
+}
