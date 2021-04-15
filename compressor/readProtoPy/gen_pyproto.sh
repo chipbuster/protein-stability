2ae776315c976e9078ed83c7e5da523565f20161
Kevin Song <chips@ksong.dev>
2021-04-14 22:49:07 -0500

    Delete proto readers

--- compressor/readProtoPy/gen_pyproto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-out_dir="$(pwd)"
-src_dir="$(pwd)"
-
-protoc -I=$src_dir --python_out=$out_dir $src_dir/deflateinfo.proto
-protoc -I=$src_dir --python_out=$out_dir $src_dir/lz77info.proto
