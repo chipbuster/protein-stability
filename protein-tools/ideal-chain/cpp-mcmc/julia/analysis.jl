4a1aac86e5b2e4ea73c672df16a49191060855cf
Kevin Song <chips@ksong.dev>
2021-08-12 18:04:45 -0500

    Add autocorrelation calculation code

---
+++ protein-tools/ideal-chain/cpp-mcmc/julia/analysis.jl
@@ -0,0 +1,24 @@
+function sim_autocov(mydat)
+    (nangles, nsteps) = size(mydat)
+
+    # Make each individual frame zero-mean
+    mydat = mydat .- mean(mydat; dims=1)
+
+    vals = 100:100:2000
+    covs = Vector{Float64}(undef, length(vals))
+
+    @inbounds @threads for index in 1:length(vals)
+        lag = vals[index]
+        mycov = 0.0
+        ctr = 0
+        @inbounds for i in 1:nsteps - lag
+            x = vec(@view mydat[:,i])
+            y = vec(@view mydat[:,i+lag])
+            mycov += dot(x,y)
+            ctr += 1
+        end
+        println(mycov, "   " ,lag)   # Crude progress meter
+        covs[index] = mycov / ctr
+    end
+    return (vals,covs)
+end
